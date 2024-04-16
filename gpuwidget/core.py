49820732115cf0e2839d1cfb910bd8596b15d24c
bmpixel <pixelwenbo@gmail.com>
2024-04-16 16:33:18 +0800

    fix version number

--- gpuwidget/core.py
+++ gpuwidget/core.py
@@ -7,7 +7,7 @@ from .widgets import GPUStatsApp
 
 def is_notebook():
     try:
-        shell = get_ipython()
+        shell = get_ipython().__class__.__name__
         print("###", shell)
         if shell == "ZMQInteractiveShell":  # jupyter notebook
             return True
