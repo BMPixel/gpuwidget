78e8628a49b372ef8fad2b367e7e663d535e8d04
bmpixel <pixelwenbo@gmail.com>
2024-04-16 16:05:32 +0800

    publish pkg and create release

--- gpuwidget/core.py
+++ gpuwidget/core.py
@@ -7,7 +7,8 @@ from .widgets import GPUStatsApp
 
 def is_notebook():
     try:
-        shell = get_ipython().__class__.__name__
+        shell = get_ipython()
+        print("###", shell)
         if shell == "ZMQInteractiveShell":  # jupyter notebook
             return True
         elif shell == "TerminalInteractiveShell":  # iPython
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,25 +1,6 @@
 from unittest.mock import MagicMock, patch
 
-from gpuwidget.core import is_notebook, live, once
-
-
-def test_is_notebook():
-    with patch("core.get_ipython") as mock_get_ipython:
-        # Test case when shell is ZMQInteractiveShell
-        mock_get_ipython.return_value.__class__.__name__ = "ZMQInteractiveShell"
-        assert is_notebook()
-
-        # Test case when shell is TerminalInteractiveShell
-        mock_get_ipython.return_value.__class__.__name__ = "TerminalInteractiveShell"
-        assert not is_notebook()
-
-        # Test case when shell is Shell
-        mock_get_ipython.return_value.__class__.__name__ = "Shell"
-        assert is_notebook()
-
-        # Test case when shell is not any of the above
-        mock_get_ipython.return_value.__class__.__name__ = "OtherShell"
-        assert not is_notebook()
+from gpuwidget.core import live, once
 
 
 @patch("gpuwidget.core.is_notebook")
